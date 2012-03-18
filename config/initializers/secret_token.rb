42b40a163505a2596ee8656e219878d9a96ce37f
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-03-18 14:33:13 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chillout::Application.config.secret_token = '9d5247424de53608ed9a3c4027b8c3638df485c50768215bdfe1f33e6646eafc59ee6aa4f96c547244cc1c3106fe4f4385eecfa7efd9cf9ad477d739cce72e7d'
