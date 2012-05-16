3b3df724270182f04640734b7c80cc873d0cffe7
Pravin Mishra <diatm.pravin.it.07.27@gmail.com>
2012-05-16 20:40:33 +0530

    removed bar index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  #before_filter :getActiveBars
+  
+  #protected
+
+  #def getActiveBars
+  #  @bars_all = Bar.find(:all, :order => 'LOWER(name) asc')
+  #end
 end
